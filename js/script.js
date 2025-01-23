63091dc24392ef4674253c574887dfc0263e0c21
Rafael Santos <rafa.05125@gmail.com>
2025-01-22 23:15:50 -0300

    Function in JS with OO

--- js/script.js
+++ js/script.js
@@ -11,25 +11,26 @@ class Product {
     this.price = price;
     this.quantity = quantity;
   }
+
+  total() {
+    return this.price * this.quantity;
+  }
+
+  updatePrice(percentage) {
+    this.price = this.price * (1 + percentage / 100);
+  }
+
   toString() {
     return `${this.name}, $${this.price.toFixed(2)}, ${this.quantity}`;
   }
 }
 
-function total(product) {
-  return product.price * product.quantity;
-}
-
-function updatePrice(product, percentage) {
-  product.price = product.price * (1 + percentage / 100);
-}
-
 const p1 = new Product("Laptop", 1000.0, 5);
 const p2 = new Product("Headphones", 200.0, 2);
 
-const total1 = total(p1);
-const total2 = total(p2);
+const total1 = p1.total();
+const total2 = p2.total();
 console.log(total1, total2);
 
-updatePrice(p1, 10);
+p1.updatePrice(10);
 console.log(p1.price);
