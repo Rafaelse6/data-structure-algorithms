aab05deb8ab5767d61de88d352d0381f68ce91f6
Rafael Santos <rafa.05125@gmail.com>
2025-01-28 09:23:12 -0300

    Encapsulation

--- java-examples/data-structure-java/src/main/java/Program.java
+++ java-examples/data-structure-java/src/main/java/Program.java
@@ -4,7 +4,10 @@ public class Program {
         var p1 = new Product("Laptop", 1000.0, 5);
         var p2 = new Product("Headphones", 200.0, 2);
 
-        System.out.println(p1);
-        System.out.println(p2);
+        p1.setName("Novo nome");
+
+        System.out.println(p1.getPrice());
+        System.out.println(p1.getName());
+
     }
 }
