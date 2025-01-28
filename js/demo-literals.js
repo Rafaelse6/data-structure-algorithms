86c70bbde31ff189f424d3c59bc7df4076750d86
Rafael Santos <rafa.05125@gmail.com>
2025-01-28 13:35:46 -0300

    Simple literal and concatenation in JS

---
+++ js/demo-literals.js
@@ -0,0 +1,15 @@
+const name = "Alice";
+const age = 31;
+const points = 8.347;
+
+// Simple literal
+const str1 = "Olá, mundo!";
+const str1_2 = "Olá mundo!";
+const str1_3 = `Olá mundo!`;
+console.log("str1:");
+console.log(str1);
+
+// Concatenation
+const str2 = "Cliente " + name + " tem idade " + age + " anos e fez " + points + " pontos.";
+console.log("str2:");
+console.log(str2);
